025fcb800c3cdbcd81aebd7495d36243b0edb464
zangetsu <dev.mehulsaini@gmail.com>
2024-04-09 19:07:43 +0530

    -

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get("/", (req, res) => res.json({message:"Express on Vercel"}));
+app.use(cors());
+
+app.get("/", (req, res) => res.json({ message: "Express on Vercel" }));
 
 app.listen(5000, () => console.log("Server ready on port 3000."));
 
