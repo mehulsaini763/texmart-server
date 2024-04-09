edaf83d47e8071e2e1565f907484af6249b3bef6
zangetsu <dev.mehulsaini@gmail.com>
2024-04-09 14:54:08 +0530

    -

--- config/auth0/config.js
+++
@@ -1,8 +0,0 @@
-export const authConfig = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: '6XZGJcgjpXRDmFb0t8HlB0yQD07ZcKWA',
-  issuerBaseURL: 'https://dev-7kus7ld03fi2xz41.us.auth0.com'
-};
